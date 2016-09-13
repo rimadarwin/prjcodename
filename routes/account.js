44c6933965297d8802ff458c3d84cc2318cdb26e
rimadarwin <rimadarwin@gmail.com>
2016-09-13 16:43:22 +0200

    commit

--- routes/account.js
+++ routes/account.js
@@ -10,29 +10,7 @@ var moment = require('moment');
 var original = './public/uploaded/files';
 var thumb = './public/uploaded/thumb';
 var size = 100;
-
-var args = [
-   original,
-   '-filter',
-   'Triangle',
-   '-define',
-   'filter:support=2',
-   '-thumbnail',
-   size,
-   '-unsharp 0.25x0.25+8+0.065',
-   '-dither None',
-   '-posterize 136',
-   '-quality 82',
-   '-define jpeg:fancy-upsampling=off',
-   '-define png:compression-filter=5',
-   '-define png:compression-level=9',
-   '-define png:compression-strategy=1',
-   '-define png:exclude-chunk=all',
-   '-interlace none',
-   '-colorspace sRGB',
-   '-strip',
-   thumb
-];
+var ratio = 0;
 
 /* GET user account details. */
 router.get('/', function(req, res) {
@@ -41,14 +19,22 @@ router.get('/', function(req, res) {
   console.log(start);
   var finish = thumb + "/" + photo;
   lw.open(start, function(err, image){
-    console.log(err||'apri');
+    var widthRatio =  width / image.width();
+    var heightRatio = height / image.height();
+    ratio = Math.max(widthRatio, heightRatio);
+    console.log(err||'width: '+image.width());
+    console.log(err||'height: '+image.height());
     if (err) throw err;
-    image.scale(0.5, function(err, image){
+    image.scale(ratio, function(err, image){
       console.log(err||'scala');
       if (err) throw err;
-      image.writeFile(finish, function(err) {
-        console.log(err||'fatto');
+      image.crop(size, size, function(err, image){
+        console.log(err||'crop');
         if (err) throw err;
+        image.writeFile(finish, function(err) {
+          console.log(err||'fatto');
+          if (err) throw err;
+        });
       });
     });
   });
